4cfe987312320c93beaf234d6870cdddb688094a
John He <hfzzhongguo@hotmail.com>
2019-04-18 16:32:40 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include"bits.stdc++.h"
+
+using namespace std;
+
+
+
+int main(){
+	printf("-$ ");scanf("%s",s);cout<<endl;
+	while(!strcmp(s,"mySQL"))printf("-$ "),scanf("%s",s),cout<<endl;
+	printf("(mysql)==> ");scanf("%s",s);cout<<endl;
+	while(!strcmp(s))
+	return 0;
+}
